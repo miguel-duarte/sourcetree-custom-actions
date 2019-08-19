bcf7b31bf762408b9086f3ff0f87dca35c066779
miguel-duarte <34094160+miguel-duarte@users.noreply.github.com>
2019-08-19 17:22:24 +0100

    Update copy_tag.sh

--- commands/copy_tag.sh
+++ commands/copy_tag.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-result=$(git describe --exact-match $1)
-echo $result | clip
+result=$(git log --decorate=short $1 | head -n1)
+echo "original result " $result
+IFS=','
+read -ra ADDR <<< "$result"
+array=()
+for i in "${ADDR[@]}"; do
+	if [[ $i == *"tag: "* ]]; then
+		array+="$i"
+	fi
+done 
+unset IFS
+
+delimiter="tag: "
+finalArray=""
+for k in "$array"; do
+	str=$k$delimiter
+	j=0;
+	while [[ $str ]]; do
+		if [[ $j != 0 && $k == *"tag: "* ]]; then
+			finalArray+="${str%%"$delimiter"*}"
+		fi
+		j+=1
+		str=${str#*"$delimiter"};
+	done;
+done
+declare -p finalArray
+echo $finalArray | clip
